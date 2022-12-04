f4e8b299dcd647261b803fa75cce4eabeaa8dea8
Sriraghavan Subramanian <subramanian.sriraghavan@gmail.com>
2022-12-04 13:42:15 +0800

    day 4

---
+++ 4-1/code.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+main();
+
+function main() {
+  const inputLines = readInputAsLines();
+  
+  let res = 0;
+  for(let i=0; i<inputLines.length; i++) {
+    let line = inputLines[i];
+    let pairs = line.split(",");
+    const [start1, end1] = pairs[0].split("-").map(a => parseInt(a));
+    const [start2, end2] = pairs[1].split("-").map(a => parseInt(a));
+
+    
+    if((start2 >= start1 && end2 <= end1) || (start1 >= start2 && end1 <= end2)) {
+      res++;
+    }
+  }
+  
+  console.log(res);
+}
+
+
+function readInputAsLines() {
+  try {
+    const data = fs.readFileSync(__dirname+'\\input', 'utf8');
+    const lines = data.split("\n").map(line => line.trim());
+    return lines;
+  } catch (err) {
+    console.error(err);
+  }
+  return [];
+}
